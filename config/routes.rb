cdfeddb6de7d6fc520618c023d96aaff5d139c54
Sara Spink <spinkbot@gmail.com>
2017-12-19 09:06:53 -0800

    create a welcome page to avoid too_many_redirects_error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root :to => 'comics#index'
+  root :to => 'welcome#index'
+  resources :welcome, :only => :index
   resources :comics
   resources :pages
 

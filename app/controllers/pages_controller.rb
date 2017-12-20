3ce7b12903bfa0fffba112ab08dcf1eb16eee996
Sara Spink <spinkbot@gmail.com>
2017-12-20 14:34:39 -0800

    create pages_controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,52 @@
+class PagesController < ApplicationController
+  before_action :authorize, only: [:new, :edit]
+  # before_action :authorize_admin, only: [:new, :edit]
+  def index
+    @page = Page.all
+  end
+
+  def show
+    @page =Page.find(params[:id])
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def create
+    @page = current_user.pages.create(page_params)
+
+      if @page.save
+        flash[:notice] = "Page Successfully Added"
+        redirect_to pages_path
+      else
+        render :new
+      end
+    end
+
+  def edit
+    @page = Page.find(params[:id])
+  end
+
+  def update
+    @page = Page.find(params[:id])
+    if @page.update(page_params)
+      redirect_to pages_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @page = Page.find(params[:id])
+    @page.destroy
+    redirect_to pages_path
+  end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:comic_id, :number, :image)
+    end
+
+end

46421224f34976a9646de9cbae1393e2fb8fb9ba
Sara Spink <spinkbot@gmail.com>
2017-12-20 15:04:22 -0800

    create new method in controller and new erb for page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,47 +1,49 @@
 class PagesController < ApplicationController
-  before_action :authorize, only: [:new, :edit]
+  # before_action :authorize, only: [:new, :edit]
   # before_action :authorize_admin, only: [:new, :edit]
   def index
+    @comic =Comic.find(params[:comic_id])
     @page = Page.all
   end
 
-  def show
-    @page =Page.find(params[:id])
-  end
-
+#   def show
+#     @page =Page.find(params[:id])
+#   end
+#
   def new
+    @comic =Comic.find(params[:comic_id])
     @page = Page.new
   end
-
-  def create
-    @page = current_user.pages.create(page_params)
-
-      if @page.save
-        flash[:notice] = "Page Successfully Added"
-        redirect_to pages_path
-      else
-        render :new
-      end
-    end
-
-  def edit
-    @page = Page.find(params[:id])
-  end
-
-  def update
-    @page = Page.find(params[:id])
-    if @page.update(page_params)
-      redirect_to pages_path
-    else
-      render :edit
-    end
-  end
-
-  def destroy
-    @page = Page.find(params[:id])
-    @page.destroy
-    redirect_to pages_path
-  end
+#
+# #   def create
+# #     @page = current_user.pages.create(page_params)
+# # binding.pry
+# #       if @page.save
+# #         flash[:notice] = "Page Successfully Added"
+# #         redirect_to pages_path
+# #       else
+# #         render :new
+# #       end
+# #     end
+#
+#   def edit
+#     @page = Page.find(params[:id])
+#   end
+#
+#   def update
+#     @page = Page.find(params[:id])
+#     if @page.update(page_params)
+#       redirect_to pages_path
+#     else
+#       render :edit
+#     end
+#   end
+#
+#   def destroy
+#     @page = Page.find(params[:id])
+#     @page.destroy
+#     redirect_to pages_path
+#   end
 
   private
 
--- app/models/page.rb
+++ app/models/page.rb
@@ -3,5 +3,5 @@ class Page < ActiveRecord::Base
   validates :number, :presence => true
 
   has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "100x100>" }, :default_url => "/img/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end

a5cc897e12830211593c975c2c57e86aa2b55718
Sara Spink <spinkbot@gmail.com>
2017-12-20 13:59:11 -0800

    add declaration in page model to allow file attachment and fix both comic and page so that thumbnails do not have to be square

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ActiveRecord::Base
   belongs_to :comic
   validates :number, :presence => true
+
+  has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end

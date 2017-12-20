3f5fc6b356320b2423860b00de44a9966bd3a94b
Sara Spink <spinkbot@gmail.com>
2017-12-20 14:02:58 -0800

    fix path to default image in page model

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,6 +2,6 @@ class Page < ActiveRecord::Base
   belongs_to :comic
   validates :number, :presence => true
 
-  has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "100x100" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "500x500>", :thumb => "100x100" }, :default_url => "/img/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
